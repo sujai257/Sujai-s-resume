62e08b35f46ca525962afb9b177987abb3d58c05
sujai257 <sujaisujai257@gmail.com>
2023-08-13 08:13:26 +0530

    .

--- script.js
+++ script.js
@@ -1,55 +1,85 @@
-// Particle.js Configuration
-particlesJS("particles-js", {
-  particles: {
-    number: { value: 80, density: { enable: true, value_area: 800 } },
-    color: { value: "#000" },
-    shape: {
-      type: "circle",
-      stroke: { width: 0, color: "#000000" },
-      polygon: { nb_sides: 5 },
+document.addEventListener("DOMContentLoaded", function () {
+  const skillItems = document.querySelectorAll(".skill-item");
+
+  skillItems.forEach((item) => {
+    item.addEventListener("mouseenter", () => {
+      const skillLevel = item.dataset.level;
+      item.style.backgroundColor = getColorForSkillLevel(skillLevel);
+    });
+
+    item.addEventListener("mouseleave", () => {
+      item.style.backgroundColor = "";
+    });
+  });
+
+  function getColorForSkillLevel(level) {
+    if (level === "Advanced") {
+      return "#51af23";
+    } else if (level === "Intermediate") {
+      return "#f39c12";
+    } else if (level === "Basics") {
+      return "#3498db";
+    } else if (level === "Beginner") {
+      return "#e74c3c";
+    }
+  }
+
+  const animatedElements = document.querySelectorAll(".animated");
+  animatedElements.forEach((element) => {
+    element.classList.add("fadeInUp");
+  });
+  particlesJS("particles-js", {
+    particles: {
+      number: { value: 100, density: { enable: true, value_area: 800 } },
+      color: { value: "#3498db" },
+      shape: {
+        type: "circle",
+        stroke: { width: 0, color: "#000000" },
+        polygon: { nb_sides: 5 },
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#3498db",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 6,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: { enable: false, rotateX: 600, rotateY: 1200 },
+      },
     },
-    opacity: {
-      value: 0.5,
-      random: false,
-      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: { enable: true, mode: "repulse" },
+        onclick: { enable: true, mode: "push" },
+        resize: true,
+      },
+      modes: {
+        grab: { distance: 400, line_linked: { opacity: 1 } },
+        bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+        repulse: { distance: 200 },
+        push: { particles_nb: 4 },
+        remove: { particles_nb: 2 },
+      },
     },
-    size: {
-      value: 3,
-      random: true,
-      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
-    },
-    line_linked: {
-      enable: true,
-      distance: 150,
-      color: "#000",
-      opacity: 0.4,
-      width: 1,
-    },
-    move: {
-      enable: true,
-      speed: 6,
-      direction: "none",
-      random: false,
-      straight: false,
-      out_mode: "out",
-      bounce: false,
-      attract: { enable: false, rotateX: 600, rotateY: 1200 },
-    },
-  },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: { enable: true, mode: "repulse" },
-      onclick: { enable: true, mode: "push" },
-      resize: true,
-    },
-    modes: {
-      grab: { distance: 400, line_linked: { opacity: 1 } },
-      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 100, duration: 0.4 },
-      push: { particles_nb: 4 },
-      remove: { particles_nb: 2 },
-    },
-  },
-  retina_detect: true,
+    retina_detect: true,
+  });
 });
